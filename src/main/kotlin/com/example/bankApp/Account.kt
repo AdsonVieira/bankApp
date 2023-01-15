d08353421e1bfeb89858bcdbfcd30ecf75309332
Adson_Vieira <adson.vieira@gruponewway.com.br>
2023-01-14 22:37:34 -0300

    Iniciando api

---
+++ src/main/kotlin/com/example/bankApp/Account.kt
@@ -0,0 +1,14 @@
+package com.example.bankApp
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity(name = "accounts")
+data class Account(
+    @Id @GeneratedValue
+    val id: Long? = null,
+    val name: String,
+    val document: String,
+    val phone: String
+)
